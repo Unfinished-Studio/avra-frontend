6b017de0c7b77b38c811d124ad6a8752930809cc
Rhuel Garza <63617583+rhuelgarza03@users.noreply.github.com>
2024-08-23 12:28:17 -0700

    Update favoritesBanner.ts

--- src/favoritesBanner.ts
+++ src/favoritesBanner.ts
@@ -1,7 +1,5 @@
 window.Webflow ||= [];
 window.Webflow.push(async () => {
-    if (!window.location.search.includes("test")) return;
-
     const wrapper = document.querySelector<HTMLElement>("[avra-element='notification-banner-wrapper']");
     if (!wrapper) return;
 
@@ -11,12 +9,14 @@ window.Webflow.push(async () => {
     if (!member || !member.data || !member.data.customFields) return;
     console.log(member);
 
+    const userFields: any = member.data.customFields;
+
     // if favorited, display banner
     // const powersStr = member.data.customFields["super-powers"] || "";
     // const powers = powersStr.split(",").map((str) => str.trim());
     // console.log(powers);
 
-    if (!member.data.customFields["super-powers"] || member.data.customFields["super-powers"] === "") {
+    if (!userFields["super-powers"] || userFields["super-powers"] === "") {
         wrapper.classList.remove("hide");
     }
 });
