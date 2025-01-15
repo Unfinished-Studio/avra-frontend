e8e7be4c1e898afd9ccf596ad8f06ffddd4b99b8
Rhuel Garza <63617583+rhuelgarza03@users.noreply.github.com>
2025-01-15 13:31:29 -0800

    feat: add batch filters to favorites

--- src/batchTemplate.ts
+++ src/batchTemplate.ts
@@ -1,3 +1,5 @@
+import { categories } from "./global";
+
 window.Webflow ||= [];
 window.Webflow.push(async () => {
     // TEMPORARY showing new page only when using test parameter
@@ -18,6 +20,50 @@ window.Webflow.push(async () => {
     if (announceEl) {
         announceEl.click();
     }
+
+    window.fsAttributes = window.fsAttributes || [];
+    window.fsAttributes.push([
+        "cmscombine",
+        () => {
+            console.log("cmscombine loaded");
+
+            const listEl = document.querySelector<HTMLElement>("[fs-cmscombine-element='list']");
+            if (!listEl) throw new Error("No list element");
+
+            const cardEls = listEl.querySelectorAll<HTMLElement>(`[avra-category]`);
+
+            // remove empty reading time and date
+            for (const cardEl of cardEls) {
+                const readingTimeEl = cardEl.querySelector<HTMLElement>("[avra-card-data='reading-time']");
+                if (readingTimeEl && !readingTimeEl.textContent) {
+                    readingTimeEl.parentElement?.remove();
+                }
+
+                const dateEl = cardEl.querySelector<HTMLElement>("[avra-card-data='date']");
+                if (dateEl && !dateEl.textContent) {
+                    dateEl.parentElement?.remove();
+                }
+            }
+
+            const selectEl = document.querySelector<HTMLSelectElement>("[name='Filter-Content']");
+            if (!selectEl) throw new Error("No select element");
+
+            for (const category of categories) {
+                const cards = listEl.querySelectorAll<HTMLElement>(`[avra-category="${category}"]`);
+                console.log("found", cards.length, "cards for", category);
+
+                if (cards.length === 0) {
+                    const options = selectEl.options;
+                    for (let i = options.length - 1; i >= 0; i--) {
+                        if (options[i].value === category) {
+                            console.log(category, "empty, removing from select");
+                            selectEl.remove(i);
+                        }
+                    }
+                }
+            }
+        },
+    ]);
 
     // TEMPORARY using old data for nested cms collections (to bypass limit)
     // const annWpr = document.querySelector<HTMLElement>("[avra-element='announcements-wrapper']");
---
+++ src/global.ts
@@ -0,0 +1,2 @@
+export const testmode = window.location.search.includes("test");
+export const categories = ["Session Insights", "Podcasts", "Case Studies", "Wikis"];
