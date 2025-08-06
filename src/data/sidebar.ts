fc31c1fd15a8c4d943d8f79a6f4683a5509b7411
Rhuel Garza <63617583+rhuelgarza03@users.noreply.github.com>
2025-08-06 13:32:49 -0700

    feat: update wiki tags data

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -129,9 +129,9 @@ export const wikiTags: Wikitag[] = [
                         ],
                     },
                     {
-                        type: "heading",
+                        type: "item",
                         displayTitle: "Chief of Staff",
-                        title: "Considering a Chief of Staff?",
+                        title: "/case-studies/chief-of-staff",
                     },
                 ],
             },
@@ -154,39 +154,39 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Mission",
-                title: "Mission",
+                title: "1. Mission",
             },
             {
                 type: "heading",
                 displayTitle: "Strategy",
-                title: "Strategy",
+                title: "2. Strategy",
             },
             {
                 type: "heading",
                 displayTitle: "Metrics",
-                title: "Metrics",
+                title: "3. Metrics",
             },
             {
                 type: "heading",
                 displayTitle: "Communicating MSM",
-                title: "Communicating MSM",
+                title: "4. Communicating your mission, strategy, and key metrics",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Whatnot",
-                        title: "/case-studies/whatnot",
+                        title: "5. Example I: Whatnot's Mission-to-Metrics",
                     },
                     {
                         type: "heading",
                         displayTitle: "Segment",
-                        title: "/case-studies/segment",
+                        title: "5. Example II: Segment’s 100 Week Plan",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Case Studies",
-                title: "Case Studies",
+                title: "6. Case Studies",
                 subItems: [
                     {
                         type: "item",
@@ -219,49 +219,49 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Senior Executives",
-                title: "Senior Executives",
+                title: "I. Onboarding and Managing Senior Executives",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Onboarding Execs",
-                        title: "onboarding-execs",
+                        title: "Onboarding Executives",
                     },
                     {
                         type: "heading",
                         displayTitle: "Aligning Execs",
-                        title: "aligning-execs",
+                        title: "Delegating and Aligning with Executives",
                     },
                     {
                         type: "heading",
                         displayTitle: "Evaluating Performance",
-                        title: "evaluating-performance",
+                        title: "Evaluating Executive Performance",
                     },
                     {
                         type: "heading",
                         displayTitle: "Firing Execs",
-                        title: "firing-execs",
+                        title: "Firing an Executive",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Hiring a COO",
-                title: "Hiring a COO",
+                title: "II. Hiring a Chief Operating Officer (COO)",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "When to Hire",
-                        title: "when-to-hire",
+                        title: "When to Hire a COO",
                     },
                     {
                         type: "heading",
                         displayTitle: "Interviewing",
-                        title: "interviewing",
+                        title: "Interviewing a COO",
                     },
                     {
                         type: "heading",
                         displayTitle: "Interview Tactics",
-                        title: "interview-tactics",
+                        title: "Tactics for Interviewing COOs",
                     },
                 ],
             },
@@ -286,19 +286,19 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Scaling Company Culture",
-                title: "Scaling Company Culture",
+                title: "I. Scaling Company Culture & Operating Cadence",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Staying Customer Centric",
-                        title: "staying-customer-centric",
+                        title: "Examples: Tactics to move fast and maintain customer centricity",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Where to Spend Time",
-                title: "Where to Spend Time",
+                title: "II. Where should you spend your time?",
                 subItems: [
                     {
                         type: "heading",
@@ -308,29 +308,29 @@ export const wikiTags: Wikitag[] = [
                     {
                         type: "heading",
                         displayTitle: "Building Your Team",
-                        title: "building-your-team",
+                        title: "Identifying bottlenecks and making sure the company is set up to deal with them.",
                     },
                     {
                         type: "heading",
                         displayTitle: "Communicating at Scale",
-                        title: "communicating-at-scale",
+                        title: "Examples: Tactics for communicating at scale",
                     },
                     {
                         type: "heading",
                         displayTitle: "Auditing Team Health",
-                        title: "auditing-team-health",
+                        title: "Auditing function/team health.",
                     },
                     {
                         type: "heading",
                         displayTitle: "Stay Close to Customers",
-                        title: "stay-close-to-customers",
+                        title: "Staying close to customers.",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Assessing Performance",
-                title: "Assessing Performance",
+                title: "III. Assessing Your Performance",
             },
         ],
     },
@@ -341,54 +341,54 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Maintaining Velocity",
-                title: "Maintaining Velocity",
+                title: "I. Maintaining engineering velocity as you scale",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Hiring",
-                        title: "hiring",
+                        title: "1. Hire the best engineers",
                     },
                     {
                         type: "heading",
                         displayTitle: "Building a Foundation",
-                        title: "building-a-foundation",
+                        title: "2. Build a solid long-term foundation from day one",
                     },
                     {
                         type: "heading",
                         displayTitle: "Tracking Metrics",
-                        title: "tracking-metrics",
+                        title: "3. Track metrics to drive decision-making",
                     },
                     {
                         type: "heading",
                         displayTitle: "Small and Independent Teams",
-                        title: "small-and-independent-teams",
+                        title: "4. Keep teams small and independent",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Recruiting Brand",
-                title: "Recruiting Brand",
+                title: "II. Building an engineering recruiting brand",
             },
             {
                 type: "heading",
                 displayTitle: "Recruiting Machine",
-                title: "Recruiting Machine",
+                title: "III. Building an engineering recruiting machine",
             },
             {
                 type: "heading",
                 displayTitle: "Standardizing Recruiting",
-                title: "Standardizing Recruiting",
+                title: "2. Standardize your process and train interviewers",
             },
             {
                 type: "heading",
                 displayTitle: "Recruiting is Everyone's Job",
-                title: "Recruiting is Everyone's Job",
+                title: "3. Make recruiting part of everyone’s job",
             },
             {
                 type: "heading",
                 displayTitle: "Recruiting Team",
-                title: "Recruiting Team",
+                title: "4. Recruiting team",
             },
             {
                 type: "heading",
@@ -416,7 +416,7 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "People Managers",
-                title: "People Managers",
+                title: "1. Develop an effective team of people managers.",
                 subItems: [
                     {
                         type: "item",
@@ -428,7 +428,7 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Communicating Expectations",
-                title: "Communicating Expectations",
+                title: "2. Clearly communicate expectations and assess performance.",
                 subItems: [
                     {
                         type: "item",
@@ -440,7 +440,7 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Career Development",
-                title: "Career Development",
+                title: "3. Establish a framework for promotions and career development",
                 subItems: [
                     {
                         type: "item",
@@ -457,12 +457,12 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Compensation Models",
-                title: "Compensation Models",
+                title: "4. Establish a compensation model that rewards and retains talent",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "General Comp Principles",
-                        title: "general-comp-principles",
+                        title: "General Compensation Principles",
                     },
                     {
                         type: "item",
@@ -479,12 +479,12 @@ export const wikiTags: Wikitag[] = [
             {
                 type: "heading",
                 displayTitle: "Empower Your Team",
-                title: "Empower Your Team",
+                title: "5. Empower a stellar people team to support you",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Head of People",
-                        title: "head-of-people",
+                        title: "When selecting a Head of People, look for:",
                     },
                     {
                         type: "item",
@@ -507,12 +507,12 @@ export const wikiTags: Wikitag[] = [
                     {
                         type: "heading",
                         displayTitle: "Accounting",
-                        title: "Accounting",
+                        title: "Accounting and Controllership",
                     },
                     {
                         type: "heading",
                         displayTitle: "FP&A",
-                        title: "FP&A",
+                        title: "FP&A and Strategic Finance",
                     },
                     {
                         type: "heading",
@@ -522,68 +522,68 @@ export const wikiTags: Wikitag[] = [
                     {
                         type: "heading",
                         displayTitle: "Team Structure",
-                        title: "Team Structure",
+                        title: "Example: Finance Team Structure",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "First Head of Finance",
-                title: "First Head of Finance",
+                title: "Hiring your first Head of Finance",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "What to Look For",
-                        title: "What to Look For",
+                        title: "What to Look for in a Head of Finance",
                     },
                     {
                         type: "heading",
                         displayTitle: "Examples",
-                        title: "Examples",
+                        title: "First Head of Finance Examples",
                     },
                     {
                         type: "heading",
                         displayTitle: "Testing a Head of Finance",
-                        title: "Testing a Head of Finance",
+                        title: "How to Test a Head of Finance",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Financial Planning",
-                title: "Financial Planning",
+                title: "Scaling Financial Planning",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Evolution",
-                        title: "Evolution",
+                        title: "The Evolution of Financial Planning",
                     },
                     {
                         type: "heading",
                         displayTitle: "Best Practices",
-                        title: "Best Practices",
+                        title: "Financial Planning Best Practices",
                     },
                 ],
             },
             {
                 type: "heading",
                 displayTitle: "Growth Capital",
-                title: "Growth Capital",
+                title: "Raising Growth Capital",
                 subItems: [
                     {
                         type: "heading",
                         displayTitle: "Non-negotiables",
-                        title: "Non-negotiables",
+                        title: "Growth fundraising non-negotiables",
                     },
                     {
                         type: "heading",
                         displayTitle: "Investor Diligencing",
-                        title: "Investor Diligencing",
+                        title: "Growth Investor Diligence Process",
                     },
                     {
                         type: "heading",
                         displayTitle: "Faire Series C",
-                        title: "Faire Series C",
+                        title: "Example: Faire Series C - Diligence",
                     },
                 ],
             },
