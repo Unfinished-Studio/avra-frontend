d7009a42223fef10bc80049565a612f0567827e5
Rhuel Garza <63617583+rhuelgarza03@users.noreply.github.com>
2025-12-04 13:49:07 -0700

    feat: remove superpowers banner logic

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,20 +1,4 @@
 window.Webflow ||= [];
 window.Webflow.push(async () => {
-    if (!window.location.search.includes("test")) return;
-
-    // get notif banner
-    const wrapper = document.querySelector<HTMLElement>("[avra-element='notification-banner-wrapper']");
-    if (!wrapper) return;
-
-    // check superpower
-    const ms = window.$memberstackDom;
-    const member = await ms.getCurrentMember();
-    if (!member || !member.data || !member.data.customFields) return;
-    console.log(member);
-
-    // display notif banner
-    const memberFields: any = member.data.customFields;
-    if (!memberFields["super-powers"] || memberFields["super-powers"] === "") {
-        wrapper.classList.remove("hide");
-    }
+    console.log("home page");
 });
